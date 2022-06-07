15b5fc3028b2f53db49e4f99f10e74c7e835febf
Deyes <deyes1219@gmail.com>
2022-06-07 11:49:47 -0500

    este es el que estamos probando solamente

---
+++ Hola.rb
@@ -0,0 +1,7 @@
+user="Deimer"
+
+if user=="Deimer"
+	puts "Desarrollador"
+else
+	puts "Acceso Denegado"
+end
---
+++ app.js
@@ -0,0 +1,2 @@
+console.log("transformación");//probando los comentarios.
+document.write("Hola esto e sun prueba");
